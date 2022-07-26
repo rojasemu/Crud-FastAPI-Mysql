635d1256ad0f5dcd76f5a650b079d7d33c8100c8
Erik Rojas <erikrojas714@gmail.com>
2022-07-26 08:59:29 -0400

    request_response_body

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
+#Python
+from typing import Optional
+
+#Pydantic
+from pydantic import BaseModel
+
+#FastAPI
 from fastapi import FastAPI
 
 app = FastAPI()
 
+#Models
+
+class Person(BaseModel):
+    first_name : str
+    last_namet :str
+    age :int
+    is_married : Optional[bool] =None
+    hair_color : Optional[str]= None
+    
+
+
 @app.get("/")
 def home():
     return {"Hello": "World"}
+
+
+#request and response  Body
+
+
+@app.post("/person/new")
+def create_person(person : Person):
+    
+    return person
+    
