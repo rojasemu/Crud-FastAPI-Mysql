21ba102a7cc83981789d9abf4b8e58bf36a5e242
Erik Rojas <erikrojas714@gmail.com>
2022-07-28 20:46:20 -0400

    automatic_body_example

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #Python
+from doctest import Example
 from typing import Optional
 from enum import Enum
 
@@ -25,38 +26,57 @@ class Location(BaseModel):
     city: str = Field(
         ...,
         min_length=1,
-        max_length=60
+        max_length=60,
+        example ="El Tigre"
         )
     state : str = Field(
            ...,
         min_length=1,
-        max_length=60
+        max_length=60,
+        example ="Anzoategui"
         )
     country : str = Field(
            ...,
         min_length=1,
-        max_length=60
+        max_length=60,
+        example ="Venezuela"
         )
 
 class Person(BaseModel):
     first_name : str = Field (
         ...,
         min_length=1,
-        max_length=50 
+        max_length=50,
+        example ="Erik" 
         )
     last_namet :str =Field(
         ...,
         min_length=1,
-        max_length=50
+        max_length=50,
+        example = "Rojas"
         )
     age :int = Field(
         ...,
         gt=0,
-        le=115
+        le=115,
+        example = 36
+        
         )
-    is_married : Optional[bool] = Field(default=None)
-    hair_color : Optional[HairColor]= Field (default=None)
+    is_married : Optional[bool] = Field(default=None, example = False)
+    hair_color : Optional[HairColor]= Field (default=None, example = "black")
     
+    
+    
+    # class Config: # esta subclase nos permite tener un valores por default en el body
+    #     schema_extra = {
+    #         "example": {
+    #             "first_name": "Facundo",
+    #             "last_name": "García Martoni",
+    #             "age": 21, 
+    #             "hair_color": "blonde",
+    #             "is_married": False
+    #         }
+    #     }
 
 
 @app.get("/")
